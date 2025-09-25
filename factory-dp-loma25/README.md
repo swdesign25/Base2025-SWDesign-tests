8904e7248690e83861a092c4117340f6ee3e02e2
Petteri Jekunen <petteri.jekunen@tuni.fi>
2025-09-25 15:22:10 +0300

    Added tests for factory dp tla

---
+++ factory-dp-loma25/README.md
@@ -0,0 +1,2 @@
+# Base2025-Factory-Design-Pattern-tests
+Tests for the student projects for GitHub Classroom 2025 - Factory Pattern task
---
+++ factory-dp-loma25/maven-test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+echo "Running the original Maven tests ========================"
+
+BASE_REPO="https://github.com/swdesign25/Base2025-Factory-Holiday.git"
+PKG_PATH=" src/test/java/fi/sade25/tla/observer/holidayfactory"
+
+echo " -> 1) clone the original base repository"
+git clone $BASE_REPO .codetest
+
+echo " -> 2) copy the original tests in place"
+cp .codetest/$PKG_PATH/*.java $PKG_PATH/.
+
+find src -name "*.java" -exec cat {} \;
+
+echo " -> 3) run the tests"
+mvn test
+
