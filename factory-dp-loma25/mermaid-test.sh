8431451410532297960e0a159fdcff12b260e54a
Petteri Jekunen <petteri.jekunen@tuni.fi>
2025-09-25 16:02:06 +0300

    Added Mermaid test

---
+++ factory-dp-loma25/mermaid-test.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Collect information ...
+
+TARGET=5
+SQFILE=docs/factory-sequence.mermaid
+CLFILE=docs/factory-class.mermaid
+
+# No test, initialize saldo
+SALDO=1
+RESULT="RESULT: $SALDO"
+
+# Seq diagram
+grep -iq factory $SQFILE 
+if [ $? -eq 0 ]; then SALDO=$(($SALDO+1)); fi
+RESULT="$RESULT - $SALDO"
+
+# Seq diagram
+grep -iq holiday $SQFILE 
+if [ $? -eq 0 ]; then SALDO=$(($SALDO+1)); fi
+RESULT="$RESULT - $SALDO"
+
+# Seq diagram
+egrep -iqe 'hotel|museum|flight|city|budget' $SQFILE
+if [ $? -eq 0 ]; then SALDO=$(($SALDO+1)); fi
+RESULT="$RESULT - $SALDO"
+
+# Class diagram
+grep -iq factory $CLFILE
+if [ $? -eq 0 ]; then SALDO=$(($SALDO+1)); fi
+RESULT="$RESULT - $SALDO"
+
+# Class diagram
+grep -iq holiday $CLFILE
+if [ $? -eq 0 ]; then SALDO=$(($SALDO+1)); fi
+RESULT="$RESULT - $SALDO"
+
+# Class diagram
+egrep -iqe 'hotel|museum|flight|city|budget' $CLFILE 
+if [ $? -eq 0 ]; then SALDO=$(($SALDO+1)); fi
+RESULT="$RESULT - $SALDO"
+
+if [ $SALDO -eq $TARGET ]; then
+     echo "SUCCESS($RESULT)"
+     exit 0
+  else
+     echo "FAILED($RESULT)"
+     exit 1
+fi
